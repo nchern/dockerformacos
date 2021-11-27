30d85b396bdb37acbf0ac56af47d0a4de65a13a4
nchern <nchern@inbox.ru>
2021-11-27 12:59:29 +0100

    add minimal usable version

---
+++ README.md
@@ -0,0 +1,17 @@
+# Docker for MacOS
+**Experimental**
+
+Docker Desktop has become really bloated.
+This is a thin frontend to docker running inside a VM.
+
+
+## Installation
+
+Using VirtualBox
+
+1. Create virtual machine named "docker".
+2. Install Alpine.
+3. Setup networking with the host.
+4. Share host file system root. Set `Folder name` to `dockerhome`.
+5. Make guest accessible via ssh from the host; add your public key to `/root/.ssh/authorized_keys`
+6. Run installation script in the guest.
---
+++ alpine-install-pkg.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+# enable test and community repo
+vi /etc/apk/repositories 
+
+apk update
+apk add docker \
+	virtualbox-guest-additions \
+	bash
+
+# enable vbox guest additions service
+rc-update add virtualbox-guest-additions
+rc-service virtualbox-guest-additions start
+rc-status
+
+# add docker user
+# NOT NEEDED? adduser dock
+
+DOCKER_HOME="/dockerhome"
+
+mkdir "$DOCKER_HOME"
+
+cp /etc/fstab ./fstab.old
+echo "share           $DOCKER_HOME     vboxsf  defaults        0       0" >> /etc/fstab
